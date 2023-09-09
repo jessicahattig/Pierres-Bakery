5334766c20da268944aaea62ee253faaa6dc70ce
jessicahattig <jessicahattig@gmail.com>
2023-09-09 12:35:28 -0700

    BreadTests.cs third test, red phase.

--- PierresBakery.Tests/ModelTests/BreadTests.cs
+++ PierresBakery.Tests/ModelTests/BreadTests.cs
@@ -30,13 +30,26 @@ namespace PierresBakery.Tests
     public void CalculateCost_Buy2Get1Free()
     {
       //Arrange
-      Bread bread = new Bread { Quantity = 3};
+      Bread bread = new Bread { Quantity = 3 };
 
       //Act
       double cost = bread.CalculateCost();
 
       //Assert
       Assert.AreEqual(10.0, cost, 0.001);
+    }
+
+    [TestMethod]
+    public void CalculateCost_Buy4loaves()
+    {
+      //Arrange
+      Bread bread = new Bread { Quantity = 4 };
+
+      //Act
+      double cost = bread.CalculateCost();
+
+      //Assert
+      Assert.AreEqual(15.0, cost, 0.001);
     }
   }
 }
