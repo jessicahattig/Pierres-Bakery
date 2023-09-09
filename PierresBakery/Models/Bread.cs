8966cee2a1f0368e88bab370228d7c3e705e847d
jessicahattig <jessicahattig@gmail.com>
2023-09-09 12:24:17 -0700

    BreadTests.cs first test, green phase.

--- PierresBakery/Models/Bread.cs
+++ PierresBakery/Models/Bread.cs
@@ -2,8 +2,15 @@ using System.Collections.Generic;
 
 namespace PierresBakery.Models
 {
-  public class Bread
-  {
+    public class Bread
+    {
+        public int Quantity { get; set; }
 
-  }
+        public double CalculateCost()
+        {
+            int set = Quantity;
+            double cost = set * 5;
+            return cost;
+        }
+    }
 }
