017c59090187fa420745582b1e372b9e791f66ef
jessicahattig <jessicahattig@gmail.com>
2023-09-08 20:12:19 -0700

    Create new project.

---
+++ PierresBakery/Models/Pastry.cs
@@ -0,0 +1,7 @@
+namespace PierresBakery.Models
+{
+  public class Pastry
+  {
+
+  }
+}
---
+++ PierresBakery/PierresBakery.Tests/ModelTests/PastryTests.cs
@@ -0,0 +1,4 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PierresBakery.Models;
+using System.Collections.Generic;
+using System;
